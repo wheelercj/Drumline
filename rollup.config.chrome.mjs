43156180f18dc6848b7a62cb6b440024195800dc
wheelercj <chrisw289@gmail.com>
2025-01-29 11:20:48 -0800

    Initial commit

---
+++ rollup.config.chrome.mjs
@@ -0,0 +1,98 @@
+/*
+   Copyright 2024 Chris Wheeler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// https://rollupjs.org/
+import copy from 'rollup-plugin-copy'; // https://www.npmjs.com/package/rollup-plugin-copy
+import del from 'rollup-plugin-delete'; // https://www.npmjs.com/package/rollup-plugin-delete
+
+// This is a Rollup configuration file for building the Chrome extension. It first
+// copies all the necessary files from `src` to the `chrome` directory, then replaces
+// all imports with the code the imports correspond to. Lastly, it deletes all files in
+// the `chrome` directory that were imported into other files there and are no longer
+// needed.
+//
+// Each time this runs, any existing files with the same names as those copied are
+// overwritten.
+
+export default [
+    {
+        input: 'chrome/background.js',
+        output: {
+            file: 'chrome/background.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+        plugins: [
+            copy({
+                targets: [
+                    {
+                        src: [
+                            // Copy everything from `src` to `chrome` except the
+                            // browserSpecific.js that is for testing.
+                            'src/*',
+                            '!src/browserSpecific.js',
+                        ],
+                        dest: 'chrome',
+                    },
+                ],
+                hook: 'buildStart', // run the copy before the build starts
+            }),
+        ]
+    },
+    {
+        input: 'chrome/content.js',
+        output: {
+            file: 'chrome/content.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+    },
+    {
+        input: 'chrome/popup.js',
+        output: {
+            file: 'chrome/popup.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+    },
+    {
+        input: 'chrome/settings.js',
+        output: {
+            file: 'chrome/settings.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+        plugins: [
+            del({
+                // Delete all files in the `chrome` directory that were imported into
+                // other files there and are no longer needed.
+                targets: [
+                    'chrome/*', // delete all files except the ones below
+
+                    '!chrome/images',
+
+                    '!chrome/manifest.json',
+
+                    '!chrome/**/*.html',
+                    '!chrome/**/*.css',
+
+                    '!chrome/browserSpecific.js',
+                    '!chrome/background.js',
+                    '!chrome/content.js',
+                    '!chrome/popup.js',
+                    '!chrome/settings.js',
+                ],
+                hook: 'buildEnd', // run the delete after the build ends
+            })
+        ]
+    }
+];
---
+++ rollup.config.firefox.mjs
@@ -0,0 +1,126 @@
+/*
+   Copyright 2024 Chris Wheeler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// https://rollupjs.org/
+import copy from 'rollup-plugin-copy'; // https://www.npmjs.com/package/rollup-plugin-copy
+import del from 'rollup-plugin-delete'; // https://www.npmjs.com/package/rollup-plugin-delete
+
+// This is a Rollup configuration file for building the Firefox extension. It first
+// copies all the necessary files from `src` to the `firefox` directory, then replaces
+// all imports with the code the imports correspond to. Lastly, it deletes all files in
+// the `firefox` directory that were imported into other files there and are no longer
+// needed.
+//
+// Each time this runs, any existing files with the same names as those copied are
+// overwritten.
+
+/**
+ * transform changes a js file to make it compatible with Firefox.
+ * @param {Buffer} contents - a file's contents.
+ * @param {string} filename - a file's name.
+ * @returns {any}
+ */
+function transform(contents, filename) {
+    if (!filename.endsWith('.js')) {
+        return contents;
+    }
+
+    return contents
+        .toString()
+        .replace(
+            // Remove all `browser` imports because firefox/browserSpecific.js doesn't
+            // define `browser` because Firefox already has a global `browser` variable.
+            /(import \{.*?)browser,?(.*?\} from ['"])/,
+            '$1$2',
+        );
+}
+
+export default [
+    {
+        input: 'firefox/background.js',
+        output: {
+            file: 'firefox/background.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+        plugins: [
+            copy({
+                // Copy all files and folders to the firefox folder except the
+                // browserSpecific.js that is for testing. There are multiple targets
+                // because the tranform function must only be called on JavaScript
+                // files.
+                targets: [
+                    {
+                        // copy the images folder and html & css files
+                        src: ['src/images', 'src/**/*.html', 'src/**/*.css'],
+                        dest: 'firefox',
+                    },
+                    {
+                        // copy js files and transform them
+                        src: ['src/**/*.js', '!src/browserSpecific.js'],
+                        dest: 'firefox',
+                        transform: transform,
+                    },
+                ],
+                flatten: false, // don't combine all nested folders into one flat folder
+                hook: 'buildStart', // run the copy before the build starts
+            }),
+        ]
+    },
+    {
+        input: 'firefox/content.js',
+        output: {
+            file: 'firefox/content.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+    },
+    {
+        input: 'firefox/popup.js',
+        output: {
+            file: 'firefox/popup.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+    },
+    {
+        input: 'firefox/settings.js',
+        output: {
+            file: 'firefox/settings.js',
+            format: 'iife', // immediately-invoked function expression
+        },
+        plugins: [
+            del({
+                // Delete all files in the `firefox` directory that were imported into
+                // other files there and are no longer needed.
+                targets: [
+                    'firefox/*', // delete all files except the ones below
+
+                    '!firefox/images',
+
+                    '!firefox/manifest.json',
+
+                    '!firefox/**/*.html',
+                    '!firefox/**/*.css',
+
+                    '!firefox/browserSpecific.js',
+                    '!firefox/background.js',
+                    '!firefox/content.js',
+                    '!firefox/popup.js',
+                    '!firefox/settings.js',
+                ],
+                hook: 'buildEnd', // run the delete after the build ends
+            })
+        ]
+    }
+];
